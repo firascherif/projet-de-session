3a849e68c9b9be6adf0b70061904cb0472abbd44
firascherif <firas-cherif@hotmail.com>
2015-11-29 23:19:38 -0800

    stars

--- js/game.js
+++ js/game.js
@@ -72,9 +72,9 @@ MyGame.Game.prototype = {
 
         for (var i = 0; i < 12; i++)
         {
-            this.star = this.stars.create(i * 70, 0, 'star');
+            this.star = this.stars.create(i * 400, 20, 'star');
             this.star.body.gravity.y = 6;
-            this.star.body.bounce.y = 0.7 + Math.random() * 0.2;
+            this.star.body.bounce.y = 0.2 + Math.random() * 0.2;
         }
 
 
